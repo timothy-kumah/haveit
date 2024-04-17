e47d7247959e4032bba49931796c7a6e46a57c7a
timothy-kumah <timothy.kumah@amalitech.com>
2024-04-17 15:11:58 +0000

    added logo

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Logo from './logo'
+import Link from 'next/link'
 
 
 const Navbar = () => {
@@ -10,9 +11,14 @@ const Navbar = () => {
         {title:"Studio",href:"/studio"}
     ]
   return (
-    <div>
+    <div className='w-full shadow-sm text-ellipsis ' >
         <div>
             <Logo title="Have it" className="text-blue-400"/>
+            {navigation.map((item)=>(
+              <Link key={item?.title} href={item?.href}>
+                {item?.title}
+              </Link>
+            ))}
         </div>
     </div>
   )
--- src/app/components/logo.tsx
+++ src/app/components/logo.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import Link from "next/link";
 
-const logo = ({title,className}) => {
+interface Props{
+  title?:string;
+  className:string;
+}
+const logo = ({title,className}:Props) => {
   return (
-    <h1 className={`text-3xl font-extrabold uppercase ${className}`}>{title}</h1>
+    <Link href={"/"}>
+          <h1 className={`text-3xl font-extrabold uppercase ${className}`}>{title||'Blog name'}</h1>
+    </Link>
   )
 }
 
