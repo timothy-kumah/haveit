125962b40dcdc88a71590b96f39ad1ef73f09886
timothy-kumah <timothy.kumah@amalitech.com>
2024-04-18 09:45:21 +0000

    Added Hero Section

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -6,9 +6,11 @@ const Hero = () => {
         <Image src={banner} 
         alt="banner image" 
         className="w-full max-h-screen object-contain"/>
-        <div className="absolute top-0 w-full h-full bg-black/30">
-            <h2>Some Random Blog</h2>
-            <p></p>
+        <div className="absolute top-0 w-full h-full
+         bg-black/30 flex flex-col items-center 
+         justify-center text-gray-100">
+            <h2 className="text-4xl md:text-7xl lg:text-[100px] font-bold">Some Random Blog</h2>
+            <p className="text-lg md:text-2xl">Content Creator, Influencer</p>
         </div>
     </div>
   )
